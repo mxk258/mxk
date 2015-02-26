650f5275eb01d994822ad1a5c7b82916b1425238
mxk <1005682757@qq.com>
2015-02-26 10:02:40 +0800

    删除文章

--- project-blog/routes/index.js
+++ project-blog/routes/index.js
@@ -228,6 +228,19 @@ module.exports = function (app) {
             res.redirect(url);
         });
     });
+
+    app.get("/remove/:name/:day/:title", checkLogin);
+    app.get("/remove/:name/:day/:title", function(req, res){
+        var currentUser = req.session.user;
+        Post.remove(currentUser.name, req.params.day, req.params.title, function(err){
+            if(err){
+                req.flash("error", err);
+                return res.redirect("back");
+            }
+            req.flash("success", "删除成功！");
+            res.redirect("/");
+        });
+    });
 };
 
 function checkLogin(req, res, next){
