b9d1ee8cd5ce0ccec2b722e5159f433d44b41060
mxk <1005682757@qq.com>
2015-02-26 15:36:23 +0800

    分页功能

--- project-blog/models/post.js
+++ project-blog/models/post.js
@@ -58,6 +58,47 @@ Post.prototype.save = function(callback){
     });
 };
 
+//一次获取十篇文章
+Post.getTen = function(name, page, callback){
+    //打开数据库
+    mongodb.open(function(err, db){
+        if(err){
+            return callback(err);
+        }
+        //读取posts集合
+        db.collection("posts", function(err, collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            var query = {};
+            if(name){
+                query.name = name;
+            }
+            //使用count返回特定查询的文档数total
+            collection.count(query, function(err, total){
+                //根据query对象查询，并跳过前(page-1)*10个结果，返回之后的10个结果
+                collection.find(query, {
+                    skip: (page - 1) * 10,
+                    limit: 10
+                }).sort({
+                    time: -1
+                }).toArray(function(err, docs){
+                    mongodb.close();
+                    if(err){
+                        return callback(err);
+                    }
+                    //解析markdown为html
+                    docs.forEach(function(doc){
+                        doc.post = markdown.toHTML(doc.post);
+                    });
+                    callback(null, docs, total);
+                });
+            });
+        });
+    });
+};
+
 //读取文章及相关信息
 Post.getAll = function(name, callback){
     //打开数据库
--- project-blog/routes/index.js
+++ project-blog/routes/index.js
@@ -10,13 +10,19 @@ var Comment = require("../models/comment.js");
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        Post.getAll(null, function(err, posts){
+        //判断是否是第一页，并把请求的页数转换成number类型
+        var page = req.query.p ? parseInt(req.query.p) : 1;
+        //查询并返回page页的10篇文章
+        Post.getTen(null, page, function(err, posts, total){
             if(err){
                 posts = [];
             }
             res.render("index", {
                 title: "主页",
                 user: req.session.user,
+                page: page,
+                isFirstPage: (page - 1) === 0,
+                isLastPage: ((page - 1) * 10 + posts.length) == total,
                 posts: posts,
                 success: req.flash("success").toString(),
                 error: req.flash("error").toString()
@@ -158,14 +164,15 @@ module.exports = function (app) {
 
     //访问用户页
     app.get("/u/:name", function(req, res){
+        var page = req.query.p ? parseInt(req.query.p) : 1;
         //检查用户是否存在
         User.get(req.params.name, function(err, user){
             if(!user){
                 req.flash("error", "用户不存在！");
                 return res.redirect("/");
             }
-            //查询并返回该用户的所有文章
-            Post.getAll(user.name, function(err, posts){
+            //查询并返回该用户第page页的10篇文章
+            Post.getTen(user.name, page, function(err, posts, total){
                 if(err){
                     req.flash("error", err);
                     return res.redirect("/");
@@ -173,6 +180,9 @@ module.exports = function (app) {
                 res.render("user", {
                     title: user.name,
                     posts: posts,
+                    page: page,
+                    isFirstPage: (page - 1) == 0,
+                    isLastPage: ((page - 1) * 10 + posts.length) == total,
                     user: req.session.user,
                     success: req.flash("success").toString(),
                     error: req.flash("error").toString()
