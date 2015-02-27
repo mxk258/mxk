d2f98f10cf9107e802f99c977e3fe98250b62145
mxk <1005682757@qq.com>
2015-02-27 11:11:33 +0800

    友情链接

--- project-blog/routes/index.js
+++ project-blog/routes/index.js
@@ -163,6 +163,15 @@ module.exports = function (app) {
         res.redirect("/upload");
     });
 
+    app.get("/links", function(req, res){
+        res.render("links", {
+            title: "友情连接",
+            user: req.session.user,
+            success: req.flash("success").toString(),
+            error: req.flash("error").toString()
+        });
+    });
+
     app.get("/search", function(req, res){
         Post.search(req.query.keyword, function(err, posts){
             if(err){
